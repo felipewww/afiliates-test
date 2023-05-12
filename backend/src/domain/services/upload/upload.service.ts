c55ea23b01c1cb99f582c9e4b8ee0a01c09349a5
Felipe Barreiros <felipewww@outlook.com.br>
2023-05-12 10:28:19 -0300

    parse file and create logic to identify and separate transactions types

--- backend/src/domain/services/upload/upload.service.ts
+++ backend/src/domain/services/upload/upload.service.ts
@@ -24,12 +24,88 @@ Esses são os valores possíveis para o campo Tipo:
 | 4    | Comissão recebida | Entrada  | +     |
 * */
 
+export enum ETransaction {
+    SALE_CREATOR = 1,
+    SALE_AFFILIATE,
+    COMMISSION_PAID,
+    COMMISSION_RECEIVED
+}
+
 @Injectable()
 export class UploadService extends BaseService<string, any>{
     async handle(so?: string): Promise<any> {
         
         console.log('file text in usecase!')
-        console.log(so)
+        // console.log(so)
+        
+        const lines = so.split("\n")
+        console.log(lines)
+        
+        const sellers = {}
+        
+        for (const line of lines) {
+            
+            // ignorar linhas em branco
+            if (line === '') {
+                continue
+            }
+            
+            const type = parseInt(line[0]);
+            const date = line.slice(1,26)
+            const product = line.slice(26,56)
+            const price = line.slice(56,66)
+            const seller = line.slice(66,86)
+            
+            const priceConverted = parseInt(price) / 100
+            
+            if (!sellers[seller]) {
+                sellers[seller] = {
+                    productsSoldAsCreator: [],
+                    productsSoldAsAffiliate: [],
+                    creditsAsCreator: [],
+                    
+                    // seria a mesma coisa aqui?
+                    creditsAsAffiliate: [], //este é apenas o total vendido. O valor real recebido é apenas na comissão
+                    creditsAsAffiliateFromCommission: [],
+                    
+                    creditsTotal: 0,
+                    debitsFromCommission: []
+                }
+            }
+            
+            // todo - não permitir arquivo se:
+            // afiliado VENDEU e não recebeu comissão
+            
+            const ref = sellers[seller];
+            
+            console.log('\n');
+            switch (type){
+                case ETransaction.SALE_CREATOR:
+                    console.log(`Criador ${seller} vendeu, ENTRADA saldo (${price}) no criador`)
+                    ref.productsSoldAsCreator.push(product)
+                    ref.creditsAsCreator.push(priceConverted)
+                    break;
+                    
+                // não considera como entrada de saldo no afiliado. Apenas um registro qde que ele vendeu
+                case ETransaction.SALE_AFFILIATE:
+                    console.log(`Afiliado ${seller} vendeu, ENTRADA de saldo (${price}) no afiliado`)
+                    ref.productsSoldAsAffiliate.push(product)
+                    ref.creditsAsAffiliate.push(priceConverted)
+                    break
+                
+                case ETransaction.COMMISSION_PAID:
+                    console.log(`Criador ${seller} pagou comissão, SAIDA do saldo (${price}) do criador`)
+                    ref.debitsFromCommission.push(priceConverted)
+                    break;
+                    
+                case ETransaction.COMMISSION_RECEIVED:
+                    console.log(`Afiliado ${seller} recebeu, ENTRADA de saldo (${price}) no afiliado`)
+                    ref.creditsAsAffiliateFromCommission.push(priceConverted)
+                    break;
+            }
+        }
+        
+        console.log(sellers);
         
         return Promise.resolve(undefined);
     }
